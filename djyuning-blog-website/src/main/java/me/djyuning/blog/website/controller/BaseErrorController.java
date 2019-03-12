a7cc2806aea1c91470a3524f69009ae1776a7c9d
djyuning <djyuning@126.com>
2019-03-12 16:54:54 +0800

    修正部分错误信息；访问不存在的分类，显示错误信息；

--- djyuning-blog-website/src/main/java/me/djyuning/blog/website/controller/BaseErrorController.java
+++ djyuning-blog-website/src/main/java/me/djyuning/blog/website/controller/BaseErrorController.java
@@ -8,26 +8,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class BaseErrorController extends BaseController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        model.addAttribute("status", new ErrorType(statusCode));
-
-        Map<String, String> page = new HashMap<>();
-        page.put("title", "出错了");
-        model.addAttribute("page", page);
-
-        return "Error/index";
-    }
 
     @Override
     public String getErrorPath() {
         return "/error";
     }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        model.addAttribute("status", new ErrorType(statusCode));
+        return "Error/index";
+    }
+
+    @RequestMapping("/404")
+    public String errorNotFound(Model model) {
+        model.addAttribute("status", new ErrorType(404));
+        return "Error/index";
+    }
+
 }
--- djyuning-blog-website/src/main/java/me/djyuning/blog/website/controller/CategoryController.java
+++ djyuning-blog-website/src/main/java/me/djyuning/blog/website/controller/CategoryController.java
@@ -2,10 +2,13 @@ package me.djyuning.blog.website.controller;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import me.djyuning.blog.beans.ErrorType;
+import me.djyuning.blog.entity.Category;
 import me.djyuning.blog.entity.Contents;
 import me.djyuning.blog.service.CategoryService;
 import me.djyuning.blog.service.ContentsService;
 import me.djyuning.blog.website.utils.Pager;
+import org.apache.tomcat.jni.Error;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,7 +20,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/Category")
@@ -39,7 +45,7 @@ public class CategoryController extends BaseController {
     @GetMapping("")
     public String home(HttpServletRequest request,
                        @RequestParam(name = "page", required = false) String page,
-                       Model model) {
+                       Model model) throws IOException {
 
         // 获取分页
         int pageNow = super.pageNowParam(page);
@@ -63,8 +69,31 @@ public class CategoryController extends BaseController {
     @GetMapping("{alias}")
     public String alias(HttpServletRequest request, HttpServletResponse response,
                         @PathVariable String alias,
-                        @RequestParam(name = "p", required = false) String p,
-                        Model model) {
+                        @RequestParam(name = "page", required = false) String page,
+                        Model model) throws IOException {
+
+        // 获取分页
+        int pageNow = super.pageNowParam(page);
+
+        // 获取分类信息
+        Category category = categoryService.getByAlias(alias);
+        model.addAttribute("category", category);
+        if (category == null) {
+            return "CategoryList";
+        }
+
+        // 获取全部文章
+        PageHelper.startPage(pageNow, pageSize);
+        List<Contents> articles = contentsService.allByCid(category.getId());
+        Page articlesPaged = (Page) articles;
+        model.addAttribute("articles", articlesPaged.getResult());
+
+        // 分页设置
+        pager.setUrl("/Category");
+        pager.setPageNow(pageNow);
+        pager.setPageSize(pageSize);
+        pager.setRowsTotal(articlesPaged.getTotal());
+        model.addAttribute("pager", pager.create());
 
         return "CategoryList";
     }
